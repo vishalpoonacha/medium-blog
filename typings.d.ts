32b8df035ba6a4ce78932f5310dd5acab735ace6
vishalpoonacha <vishalpoonacha007@gmail.com>
2022-02-26 11:54:28 +0530

    commit

---
+++ typings.d.ts
@@ -0,0 +1,21 @@
+//defination tp file
+
+export interface Post {
+  _id: string;
+  _createdAt: string;
+  title: string;
+  author: {
+    name: string;
+    image: string;
+  };
+  description: string;
+  mainImage: {
+    asset: {
+      url: string;
+    };
+    slug: {
+      current: string;
+    };
+    body: [object];
+  };
+}
